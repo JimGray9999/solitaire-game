3684ae88bdfc7726a145620c8e4785d662437dc0
Jim Gray <jgray8689@gmail.com>
2026-04-05 00:08:00 -0400

    Add @vercel/speed-insights for performance monitoring
    - Included @vercel/speed-insights package to enhance performance tracking.
    - Updated package.json and package-lock.json to reflect the new dependency.
    - Integrated SpeedInsights component in main.tsx for real-time performance insights.

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import App from "./App";
 import "./styles/global.css";
 import "@fontsource/inter";
@@ -9,5 +10,6 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
     <Analytics />
+    <SpeedInsights />
   </React.StrictMode>
 );
